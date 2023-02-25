45557ba1fc272a0193a26653d8d171ad8d46e0d4
RuichangBao <1294838346@qq.com>
2023-02-25 16:00:27 +0800

    动态图集

---
+++ DynamicAtlasDemo/Assets/Scripts/ImageExpand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageExpand : Image
+{
+    private DynamicAtlas dynamicAtlas;
+    private void Start()
+    {
+        this.dynamicAtlas = GameObject.Find("Main Camera").GetComponent<DynamicAtlas>();
+        base.Start();
+        Sprite sprite = this.sprite;
+        this.sprite = dynamicAtlas.GetOrLoadSprite(sprite);
+    }
+}
---
+++ DynamicAtlasDemo/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.IO;
+
+public class Test : MonoBehaviour
+{
+    public Texture2D sourceTexture1, sourceTexture2;
+    private Texture2D destinationTexture;
+
+    void Start()
+    {
+        Debug.LogError("AAAAAAAAAA");
+        destinationTexture = new Texture2D(1024, 1024);
+        Graphics.CopyTexture(sourceTexture1, 0, 0, 0, 0, 275, 607, destinationTexture, 0, 0, 0, 0);
+        Graphics.CopyTexture(sourceTexture2, 0, 0, 0, 0, 275, 607, destinationTexture, 0, 0, 275, 0);
+
+        byte[] datas = destinationTexture.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/拷贝图片.png", datas);
+    }
+}
